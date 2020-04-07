dd6ecca3d81a7e44898fd1278deb3f1a61274eb9
Bradley Arsenault <brad@swiftlyai.com>
2020-04-06 20:21:28 -0400

    Got rid of the superflous base agent and random agent files as they were unused and just confusing.

--- kwola/components/agents/DeepLearningAgent.py
+++ kwola/components/agents/DeepLearningAgent.py
@@ -19,8 +19,7 @@
 #
 
 
-from .BaseAgent import BaseAgent
-from .  TraceNet import   TraceNet
+from .TraceNet import TraceNet
 from ...components.utilities.debug_plot import showRewardImageDebug
 from ...datamodels.ExecutionTraceModel import ExecutionTrace
 from ...datamodels.ExecutionSessionModel import ExecutionSession
@@ -67,25 +66,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-def grouper(n, iterable):
-    """Chunks an iterable into sublists"""
-    it = iter(iterable)
-    while True:
-       chunk = tuple(itertools.islice(it, n))
-       if not chunk:
-           return
-       yield chunk
-
-
-# noinspection PyUnresolvedReferences
-class DeepLearningAgent(BaseAgent):
+class DeepLearningAgent:
     """
         This class represents a deep learning agent, which uses reinforcement learning to make the automated testing more effective
     """
 
     def __init__(self, config, whichGpu="all"):
-        super().__init__(config)
-
         self.config = config
         
         self.whichGpu = whichGpu
@@ -154,6 +140,90 @@ class DeepLearningAgent(BaseAgent):
             "totalRebalancedLoss": [],
             "batchReward": []
         }
+
+        self.actions = {
+            "click": lambda x,y: ClickTapAction(type="click", x=x, y=y, times=1),
+            "clear": lambda x, y: ClearFieldAction(type="clear", x=x, y=y),
+        }
+
+        self.actionBaseWeights = [
+            config['random_weight_click'],
+            config['random_weight_clear']
+        ]
+
+        self.actionProbabilityBoostKeywords = [
+            [],
+            []
+        ]
+
+        if config['email']:
+            self.actions['typeEmail'] = lambda x,y: TypeAction(type="typeEmail", x=x, y=y, label="email", text=config['email'])
+            self.actionBaseWeights.append(config['random_weight_type_email'])
+            self.actionProbabilityBoostKeywords.append(["email", "user"])
+
+        if config['password']:
+            self.actions['typePassword'] = lambda x,y: TypeAction(type="typePassword", x=x, y=y, label="password", text=config['password'])
+            self.actionBaseWeights.append(config['random_weight_type_password'])
+            self.actionProbabilityBoostKeywords.append(["pass"])
+
+        if config['name']:
+            self.actions['typeName'] = lambda x,y: TypeAction(type="typeName", x=x, y=y, label="name", text=config['name'])
+            self.actionBaseWeights.append(config['random_weight_type_name'])
+            self.actionProbabilityBoostKeywords.append(["email", "user"])
+
+        if config['paragraph']:
+            self.actions['typeParagraph'] = lambda x,y: TypeAction(type="typeParagraph", x=x, y=y, label="paragraph", text=config['paragraph'])
+            self.actionBaseWeights.append(config['random_weight_type_paragraph'])
+            self.actionProbabilityBoostKeywords.append(["pass"])
+
+        if config['enableRandomNumberCommand']:
+            self.actions['typeNumber'] = lambda x,y: TypeAction(type="typeNumber", x=x, y=y, label="number", text=self.randomString('-.0123456789$%', random.randint(1, 5)))
+            self.actionBaseWeights.append(config['random_weight_type_number'])
+            self.actionProbabilityBoostKeywords.append(["num", "count", "int", "float"])
+
+        if config['enableDoubleClickCommand']:
+            self.actions['doubleClick'] = lambda x,y: ClickTapAction(type="double_click", x=x, y=y, times=2)
+            self.actionBaseWeights.append(config['random_weight_double_click'])
+            self.actionProbabilityBoostKeywords.append([])
+
+        if config['enableRightClickCommand']:
+            self.actions['rightClick'] = lambda x,y: RightClickAction(type="right_click", x=x, y=y)
+            self.actionBaseWeights.append(config['random_weight_right_click'])
+            self.actionProbabilityBoostKeywords.append([])
+
+        if config['enableRandomBracketCommand']:
+            self.actions['typeBrackets'] = lambda x,y: TypeAction(type="typeBrackets", x=x, y=y, label="brackets", text=self.randomString('{}[]()', random.randint(1, 3)))
+            self.actionBaseWeights.append(config['random_weight_type_brackets'])
+            self.actionProbabilityBoostKeywords.append([])
+
+        if config['enableRandomMathCommand']:
+            self.actions['typeMath'] = lambda x,y: TypeAction(type="typeMath", x=x, y=y, label="symbol", text=self.randomString('*=+<>', random.randint(1, 3)))
+            self.actionBaseWeights.append(config['random_weight_type_math'])
+            self.actionProbabilityBoostKeywords.append([])
+
+        if config['enableRandomOtherSymbolCommand']:
+            self.actions['typeOtherSymbol'] = lambda x,y: TypeAction(type="typeOtherSymbol", x=x, y=y, label="symbol", text=self.randomString('"\';:/?,!^&#@', random.randint(1, 3)))
+            self.actionBaseWeights.append(config['random_weight_type_other_symbol'])
+            self.actionProbabilityBoostKeywords.append([])
+
+        self.elementBaseWeights = {
+            "a": config['random_html_element_a_weight'],
+            "input": config['random_html_element_input_weight'],
+            "button": config['random_html_element_button_weight'],
+            "p": config['random_html_element_p_weight'],
+            "span": config['random_html_element_span_weight'],
+            "div": config['random_html_element_div_weight'],
+            "canvas": config['random_html_element_canvas_weight'],
+            "other": config['random_html_element_other_weight']
+        }
+
+        self.actionsSorted = sorted(self.actions.keys())
+
+    def randomString(self, chars, len):
+        base = ""
+        for n in range(len):
+            base += str(random.choice(chars))
+        return base
 
     def load(self):
         """
